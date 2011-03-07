006e373ff1b5a22f10db59b9e90acb8666ba3565
Oran Looney <olooney@gmail.com>
2011-03-06 21:55:58 -0600

    hard words, cleaning up licenses, DRY fonts and word data.

--- resources/typing.js
+++ resources/typing.js
@@ -1,47 +1,25 @@
-/* Font Wars 
+/* Font Wars Game Code
 Copyright 2011, Oran Looney
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+MIT License, see README
 */
 
 $(function() { 
-var words = 'a,able,about,above,across,act,action,actually,add,addition,adjective,afraid,Africa,after,again,against,age,ago,agreed,ahead,air,all,allow,almost,alone,along,already,also,although,always,am,America,among,amount,an,and,angle,animal,another,answer,any,anything,appear,apple,are,area,arms,army,around,arrived,art,as,ask,at,away,baby,back,bad,ball,bank,base,be,bear,beat,beautiful,became,because,become,bed,been,before,began,begin,behind,being,believe,bell,belong,below,beside,best,better,between,big,bill,birds,bit,black,block,blood,blow,blue,board,boat,body,bones,book,born,both,bottom,box,boy,branches,break,bright,bring,British,broken,brother,brought,brought,brown,build,building,built,burning,business,but,buy,by,call,came,can,cannot,can\'t,capital,captain,car,care,carefully,carry,case,cat,catch,cattle,caught,cause,cells,center,cents,century,certain,chance,change,chart,check,chief,child,children,choose,church,circle,city,class,clean,clear,climbed,close,clothes,cloud,coast,cold,color,column,come,common,company,compare,complete,compound,conditions,consider,consonant,contain,continued,control,cook,cool,copy,corn,corner,correct,cost,cotton,could,couldn\'t,count,country,course,covered,cows,create,cried,crops,cross,crowd,current,cut,dance,dark,day,dead,deal,death,decided,decimal,deep,describe,desert,design,details,determine,developed,dictionary,did,didn\'t,died,difference,different,difficult,direct,direction,discovered,distance,divided,division,do,doctor,does,doesn\'t,dog,dollars,done,don\'t,door,down,draw,drawing,dress,drive,drop,dry,during,each,early,ears,earth,east,easy,eat,edge,effect,eggs,eight,either,electric,elements,else,end,energy,engine,England,English,enjoy,enough,entered,entire,equal,equation,especially,Europe,even,evening,ever,every,everyone,everything,exactly,example,except,exciting,exercise,expect,experience,experiment,explain,express,eye,face,fact,factories,factors,fall,family,famous,far,farm,farmers,fast,father,fear,feel,feeling,feet,fell,felt,few,field,fig,fight,figure,filled,finally,find,fine,fingers,finished,fire,first,fish,fit,five,flat,floor,flow,flowers,fly,follow,food,foot,for,force,forest,form,forward,found,four,fraction,France,free,French,fresh,friends,from,front,fruit,full,fun,game,garden,gas,gave,general,get,girl,give,glass,go,God,gold,gone,good,got,government,grass,great,Greek,green,grew,ground,group,grow,guess,gun,had,hair,halt,hand,happened,happy,hard,has,hat,have,he,head,hear,heard,heart,heat,heavy,held,help,her,here,high,hill,him,himself,his,history,hit,hold,hole,home,hope,horse,hot,hours,house,how,however,huge,human,hundred,hunting,I,ice,idea,if,I\'ll,important,in,inches,include,increase,Indian,indicate,industry,information,insects,inside,instead,instruments,interest,interest,into,iron,is,island,isn\'t,it,its,it\'s,itself,Japanese,job,joined,jumped,just,keep,kept,key,killed,kind,king,knew,know,known,lady,lake,land,language,large,last,later,laughed,law,lay,lead,learn,least,leave,led,left,legs,length,less,let,let\'s,letter,level,lie,life,lifted,light,like,line,list,listen,little,live,located,long,look,lost,lot,loud,love,low,machine,made,main,major,make,man,many,map,march,mark,match,material,matter,may,maybe,me,mean,measure,meat,meet,melody,members,men,metal,method,middle,might,mile,milk,million,mind,mine,minutes,miss,modern,molecules,moment,money,months,moon,more,morning,most,mother,mountain,mouth,move,movement,much,music,must,my,name,nation,natural,near,necessary,need,never,new,next,night,no,nor,north,northern,nose,not,note,nothing,notice,noun,now,number,numeral,object,observe,ocean,of,off,office,often,oh,oil,old,on,once,one,only,open,opposite,or,order,other,our,out,outside,over,own,oxygen,page,paint,pair,paper,paragraph,park,part,particular,party,passed,past,pattern,pay,people,per,perhaps,period,person,phrase,picked,picture,piece,place,plains,plan,plane,plant,plants,play,please,plural,poem,point,pole,poor,position,possible,pounds,power,practice,prepared,presidents,pretty,printed,probably,problem,process,produce,products,property,provide,pulled,pushed,put,questions,quickly,quiet,quite,race,radio,rain,raised,ran,rather,reached,read,ready,really,reason,received,record,red,region,remain,remember,repeated,report,represent,resent,rest,result,return,rhythm,rich,ride,right,ring,rise,river,road,rock,rolled,room,root,rope,rose,round,row,rule,run,safe,said,sail,same,sand,sat,save,saw,say,scale,school,science,scientists,score,sea,seat,second,section,see,seeds,seem,seen,sell,send,sense,sent,sentence,separate,serve,set,settled,seven,several,shall,shape,sharp,she,ship,shoes,shop,short,should,shoulder,shouted,show,shown,side,sight,sign,signal,silent,similar,simple,since,sing,sir,sister,sit,six,size,skin,sky,sleep,sleep,slowly,small,smell,smiled,snow,so,soft,soil,soldiers,solution,some,someone,something,sometimes,son,song,soon,sound,south,southern,space,speak,special,speed,spell,spot,spread,spring,square,stand,stars,start,state,statement,stay,steel,step,stick,still,stone,stood,stop,store,story,straight,strange,stream,street,stretched,string,strong,students,study,subject,substances,such,suddenly,suffix,sugar,suggested,sum,summer,sun,supply,suppose,sure,surface,surprise,swim,syllables,symbols,system,table,tail,take,talk,tall,teacher,team,tell,temperature,ten,terms,test,than,that,the,their,them,themselves,then,there,these,they,thick,thin,thing,think,third,this,those,though,thought,thousands,three,through,thus,tied,time,tiny,to,today,together,told,tone,too,took,tools,top,total,touch,toward,town,track,trade,train,train,travel,tree,triangle,trip,trouble,truck,true,try,tube,turn,two,type,uncle,under,underline,understand,unit,until,up,upon,us,use,usually,valley,value,various,verb,very,view,village,visit,voice,vowel,wait,walk,wall,want,war,warm,was,wash,Washington,wasn\'t,watch,water,waves,way,we,wear,weather,week,weight,well,we\'ll,went,were,west,western,what,wheels,when,where,whether,which,while,white,who,whole,whose,why,wide,wife,wild,will,win,wind,window,wings,winter,wire,wish,with,within,without,woman,women,wonder,won\'t,wood,word,work,workers,world,would,wouldn\'t,write,written,wrong,wrote,yard,year,yellow,yes,yet,you,young,your,you\'re,yourself'.split(',');
 
-// fonts:
-var fonts = [
-	'Arimo',
-	'PT Serif',
-	'Dancing Script',
-	'Kreon',
-	'IM Fell DW Pica',
-	'Goudy Bookletter 1911',
-	'Buda',
-	'Bentham',
-	'Tangerine',
-	'Copse',
-	'Orbitron',
-	'Geo',
-	'Calligraffitti',
-	'Philosopher',
-	'Crimson Text',
-	'Molengo'
-];
+var words = $('#word-data').html().split(/\s+/g).filter(function(w) { return w.length; });
+
+// DRY: figure out available fonts from the HTML
+var fonts = (function() {
+	var fonts = [];
+	var fontUrlPattern = /^http:\/\/fonts.*=([a-zA-Z0-9+]+)/
+	$('link[rel=stylesheet]').each(function() {
+		var url = $(this).attr('href');
+		var match = fontUrlPattern.exec(url);
+		if ( match ) {
+			fonts.push( match[1].replace(/\+/g, ' ') );
+		}
+	});
+	return fonts;
+})();
 
 var loadingScreen = true;
 var gameOver = false;
@@ -51,21 +29,18 @@ var hits = 0;
 var misses = 0;
 var startTime = new Date();
 
-var bullets = ['.', '!', '*', '..', '!!', '**'];
+var bullets = ['.', '!', '*', '! !', '* *', '!!!', '***'];
 function setMultiplier(m) {
 	multiplier = m || 1;
 	if ( multiplier > 50 ) multiplier = 50;
 
-	if ( multiplier == 50 ) bullet = bullets[5];
-	else if ( multiplier >= 30 ) bullet = bullets[4];
-	else if ( multiplier >= 20 ) bullet = bullets[3];
-	else if ( multiplier >= 10 ) bullet = bullets[2];
+	if ( multiplier >= 10 ) bullet = bullets[ Math.floor(multiplier/10) + 1 ];
 	else if ( multiplier >= 5 ) bullet = bullets[1];
 	else bullet = bullets[0];
 }
 setMultiplier(1);
 
-sound.fx.load('hit.', 'resources/sounds/39459__THE_bizniss__laser.ogg', 1.0, 7);
+sound.fx.load('hit.', 'resources/sounds/39459__THE_bizniss__laser.ogg', 0.8, 7);
 sound.fx.load('hit!', 'resources/sounds/39456__THE_bizniss__laser_2.ogg', 1.0, 7);
 sound.fx.load('hit*', 'resources/sounds/39458__THE_bizniss__laser_4.ogg', 1.0, 7);
 sound.fx.load('miss','resources/sounds/2225__Andrew_Duke__garp.ogg', 0.5);
@@ -280,13 +255,13 @@ function spawn() {
 
 	// auto-balancing logic
 	var danger = attackingWordCount();
-	$(enemy).animate( alignCenters(spaceship, enemy), 10000 + 1000*danger - 15*hits, 'linear', function() {
+	$(enemy).animate( alignCenters(spaceship, enemy), 10000 + 1000*danger - 5*hits, 'linear', function() {
 		// the player dies when an enemy reaches the spaceship.
 		// the timeout is because we can't start the fade animation on this enemy from inside this callback.
 		if ( $.contains(document.body, this) ) setTimeout(function() { die(); }, 1);
 	});
-	var spawnInterval = 200 + 400*danger - 5*hits + 100 - 200*Math.random();
-	if ( spawnInterval < 1 ) spawnInterval = 1;
+	var spawnInterval = 200 + 400*danger - hits + 100 - 200*Math.random();
+	if ( spawnInterval < 500 ) spawnInterval = 500;
 	setTimeout(spawn, spawnInterval);
 }
 
